df9398a4442937745749c30b96323ece3f8f743b
Morteza Masumi <morteza.masumi@gmail.com>
2025-09-19 15:02:12 +0330

    fix: minor

--- src/Facades/FbEssentials.php
+++ src/Facades/FbEssentials.php
@@ -5,6 +5,11 @@ namespace Mortezamasumi\FbEssentials\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static void filamentShieldAddResource(string $class, array $permissions, bool $replace)
+ * @method static void filamentShieldExcludeResource(string $class)
+ * @method static void filamentShieldExcludePage(string $class)
+ * @method static void filamentShieldExcludeWidget(string $class)
+ *
  * @see \Mortezamasumi\FbEssentials\FbEssentials
  */
 class FbEssentials extends Facade
