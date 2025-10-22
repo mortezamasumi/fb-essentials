53e203e06fa40a8750bd23ca964d8c3a52508ff6
Morteza Masumi <morteza.masumi@gmail.com>
2025-10-22 08:14:14 +0330

    feat: fix bug in pSort

--- src/Macros/PsortMacroServiceProvider.php
+++ src/Macros/PsortMacroServiceProvider.php
@@ -4,6 +4,8 @@ namespace Mortezamasumi\FbEssentials\Macros;
 
 use Filament\Tables\Columns\Column;
 use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 use Mortezamasumi\FbEssentials\Facades\FbPersian;
@@ -30,18 +32,69 @@ class PsortMacroServiceProvider extends ServiceProvider
          */
         Collection::macro('pSort', function (?string $field = null, bool $ascending = true, ?callable $callback = null) {
             /** @var Collection $this */
-            $items = $this->all();
 
-            usort($items, function ($a, $b) use ($field, $ascending, $callback) {
-                $valueA = $field ? data_get($a, $field) : $a;
-                $valueB = $field ? data_get($b, $field) : $b;
+            // Type 4: Eloquent Collection
+            if ($this instanceof EloquentCollection) {
+                return $this;
+            }
 
+            // Type 2: Dictionary - collect(['111' => 'a', '222' => 'b']);
+            if (Arr::isAssoc($this->toArray())) {
+                $sortedItems = $this->all();
+
+                uasort($sortedItems, function ($a, $b) use ($ascending, $callback) {
+                    if ($callback) {
+                        $valueA = $callback($a);
+                        $valueB = $callback($b);
+                    } else {
+                        $valueA = strtr((string) $a, FbPersian::persianConvert());
+                        $valueB = strtr((string) $b, FbPersian::persianConvert());
+                    }
+
+                    $result = $valueA <=> $valueB;
+
+                    return $ascending ? $result : -$result;
+                });
+
+                return new static($sortedItems);
+            }
+
+            // Type 3: List of Structs - collect([['key' => '111', 'value' => 'a'], ['key' => '222', 'value' => 'b']])
+            $firstItem = $this->first();
+
+            if ($this->isNotEmpty() && (is_array($firstItem) || is_object($firstItem))) {
+                $sortedItems = $this->all();
+
+                usort($sortedItems, function ($a, $b) use ($field, $ascending, $callback) {
+                    $valueA = $field ? data_get($a, $field) : $a;
+                    $valueB = $field ? data_get($b, $field) : $b;
+
+                    if ($callback) {
+                        $valueA = $callback($valueA);
+                        $valueB = $callback($valueB);
+                    } else {
+                        $valueA = strtr($valueA, FbPersian::persianConvert());
+                        $valueB = strtr($valueB, FbPersian::persianConvert());
+                    }
+
+                    $result = $valueA <=> $valueB;
+
+                    return $ascending ? $result : -$result;
+                });
+
+                return new static($sortedItems);
+            }
+
+            // Type 1: Simple - collect(['a', 'b', 'c'])
+            $sortedItems = $this->all();
+
+            usort($sortedItems, function ($a, $b) use ($ascending, $callback) {
                 if ($callback) {
-                    $valueA = $callback($valueA);
-                    $valueB = $callback($valueB);
+                    $valueA = $callback($a);
+                    $valueB = $callback($b);
                 } else {
-                    $valueA = strtr($valueA, FbPersian::persianConvert());
-                    $valueB = strtr($valueB, FbPersian::persianConvert());
+                    $valueA = strtr((string) $a, FbPersian::persianConvert());
+                    $valueB = strtr((string) $b, FbPersian::persianConvert());
                 }
 
                 $result = $valueA <=> $valueB;
@@ -49,7 +102,7 @@ class PsortMacroServiceProvider extends ServiceProvider
                 return $ascending ? $result : -$result;
             });
 
-            return new static($items);
+            return new static($sortedItems);
         });
 
         TextColumn::mixin(new class implements PsortMacrosInterface {});
--- tests/Tests/PsortTest.php
+++ tests/Tests/PsortTest.php
@@ -1,9 +1,66 @@
 <?php
 
 it('test collection psort macro', function () {
-    expect(collect(['علی', 'یاور', 'نقی', 'تقی', 'آدم', 'حوا'])->shuffle()->pSort()->toArray())
-        ->toMatchArray(collect(['آدم', 'تقی', 'حوا', 'علی', 'نقی', 'یاور'])->toArray());
+    // Type 1: Simple - collect(['a', 'b', 'c'])
+    expect(collect([
+        'علی',
+        'یاور',
+        'نقی',
+        'تقی',
+        'آدم',
+        'حوا'
+    ])
+            ->pSort()
+            ->toArray())
+        ->toMatchArray(collect([
+            'آدم',
+            'تقی',
+            'حوا',
+            'علی',
+            'نقی',
+            'یاور'
+        ])
+            ->toArray());
 
-    expect(collect([['name' => 'علی'], ['name' => 'یاور'], ['name' => 'نقی'], ['name' => 'تقی'], ['name' => 'آدم'], ['name' => 'حوا']])->shuffle()->pSort('name')->values()->toArray())
-        ->toMatchArray(collect([['name' => 'آدم'], ['name' => 'تقی'], ['name' => 'حوا'], ['name' => 'علی'], ['name' => 'نقی'], ['name' => 'یاور']])->values()->toArray());
+    // Type 2: Dictionary - collect(['111' => 'a', '222' => 'b']);
+    expect(collect([
+        'key01' => 'علی',
+        'key02' => 'یاور',
+        'key03' => 'نقی',
+        'key04' => 'تقی',
+        'key05' => 'آدم',
+        'key06' => 'حوا'
+    ])
+            ->pSort()
+            ->toArray())
+        ->toMatchArray(collect([
+            'key05' => 'آدم',
+            'key04' => 'تقی',
+            'key06' => 'حوا',
+            'key01' => 'علی',
+            'key03' => 'نقی',
+            'key02' => 'یاور'
+        ])
+            ->toArray());
+
+    // Type 3: List of Structs - collect([['key' => '111', 'value' => 'a'], ['key' => '222', 'value' => 'b']])
+    expect(collect([
+        ['name' => 'علی', 'another-attribute' => 'another-value'],
+        ['name' => 'یاور', 'another-attribute' => 'another-value'],
+        ['name' => 'نقی', 'another-attribute' => 'another-value'],
+        ['name' => 'تقی', 'another-attribute' => 'another-value'],
+        ['name' => 'آدم', 'another-attribute' => 'another-value'],
+        ['name' => 'حوا', 'another-attribute' => 'another-value']
+    ])
+            ->pSort('name')
+            ->values()
+            ->toArray())
+        ->toMatchArray(collect([
+            ['name' => 'آدم', 'another-attribute' => 'another-value'],
+            ['name' => 'تقی', 'another-attribute' => 'another-value'],
+            ['name' => 'حوا', 'another-attribute' => 'another-value'],
+            ['name' => 'علی', 'another-attribute' => 'another-value'],
+            ['name' => 'نقی', 'another-attribute' => 'another-value'],
+            ['name' => 'یاور', 'another-attribute' => 'another-value']
+        ])->values()->toArray());
 });
